b68356585d455534334efe791560bef623dc70e5
dhruvgogdani <163969922+dhruvgogdani@users.noreply.github.com>
2024-03-23 14:27:18 +0530

    update

---
+++ 360Degree rotate/360degree/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ProductView from "./component/ProductView";
+
+function App() {
+  return (
+    <ProductView/>
+  );
+}
+
+export default App;
---
+++ 360Degree rotate/360degree/src/component/ProductView.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ThreeSixety from "react-360-view";
+
+const ProductView = () => {
+  return (
+    <div>
+      <div>
+        <ThreeSixety
+          amount={36}
+          imagepath="https://www.google.com/imgres?q=pizza%20png%20image&imgurl=https%3A%2F%2Ft4.ftcdn.net%2Fjpg%2F05%2F74%2F68%2F47%2F360_F_574684775_Vw9D3VkXofqdoeWdPkI6ORTafqA6zhqg.jpg&imgrefurl=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Dpizza%2Bpng&docid=uH7oAATXjmrbwM&tbnid=-2J0ptxZ8SijtM&vet=12ahUKEwjN-YSkzoeFAxXPsFYBHTJfClIQM3oECGgQAA..i&w=533&h=360&hcb=2&ved=2ahUKEwjN-YSkzoeFAxXPsFYBHTJfClIQM3oECGgQAA"
+          fileName=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProductView;
